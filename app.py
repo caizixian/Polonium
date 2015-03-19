5e7128f42ff46fe9ed8c69b565cb06067df582dd
lwher <751276936@qq.com>
2015-03-19 18:26:48 +0800

    3.19 18:26  orz czx

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for, flash
 import mongoengine
 import datetime
 
@@ -20,16 +20,43 @@ class Post(mongoengine.Document):
 
 class Config(mongoengine.Document):
     title = mongoengine.StringField(max_length=255, required=True)
+    name = mongoengine.StringField(max_length=255, required=True)
 
+
+DEBUG = True
+SECRET_KEY = 'development key'
 
 app = Flask(__name__)
-app.config.update({"DEBUG" : True})
+app.config.from_object(__name__)
 mongoengine.connect('polonium')
 
 
 @app.route('/')
-def show_homepage():
-    return render_template('base.html', title=Config.objects[0].title)
+def index():
+    return render_template('index.html', config=Config.objects[0] )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        try:
+            if request.form['password'] != Author.objects.get(username=request.form['username']).password:
+                error = '密码错误'
+            else:
+                session['logged_in'] = True
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+        except mongoengine.DoesNotExist:
+            error = '该用户不存在'
+    return render_template('login.html', error=error, config=Config.objects[0])
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('您已注销')
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
